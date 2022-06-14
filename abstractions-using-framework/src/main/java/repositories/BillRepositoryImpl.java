60d08460bbe0ce8067d54b87b1ed24d4ce855a7f
Mihir Sawant <mih3190Sawant@outlook.com>
2022-06-14 22:42:11 +0530

    managing multi-beans of same type and DI auto-wired

--- abstractions-using-framework/src/main/java/repositories/BillRepositoryImpl.java
+++ abstractions-using-framework/src/main/java/repositories/BillRepositoryImpl.java
@@ -1,9 +1,16 @@
 package repositories;
 
 import entity.Bill;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
-@Component
+/*
+    Marked the BillRepositoryImpl class as a Repository
+    because this is our repository for storing and managing
+    the data and perform all DB related transactions.
+    This is also a stereotype annotation, So it will create
+    and instantiate the bean in the Spring Context
+ */
+@Repository
 public class BillRepositoryImpl implements BillRepository{
     @Override
     public void storeBill(Bill bill) {
